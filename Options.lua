ec8f798295d4370e2e205cc18dab4586a67f836e
wowaddonmaker <wowaddonmaker@proton.me>
2026-03-25 15:03:36 -0400

    Fix options panel rendering behind action bars

--- Options.lua
+++ Options.lua
@@ -516,7 +516,7 @@ function Options:Initialize()
     else
         optionsFrame:SetPoint("TOP", UIParent, "TOP", 0, -100)
     end
-    optionsFrame:SetFrameStrata("MEDIUM")
+    optionsFrame:SetFrameStrata("DIALOG")
     optionsFrame:SetMovable(true)
     optionsFrame:EnableMouse(true)
     optionsFrame:SetClampedToScreen(true)
@@ -1721,7 +1721,7 @@ function Options:DoResetAll()
     EasyFind.db.enableMapSearch = true
     EasyFind.db.globalSearchFilters = { zones = true, dungeons = true, raids = true, delves = true }
     EasyFind.db.localSearchFilters = { instances = true, travel = true, services = true }
-    EasyFind.db.uiSearchFilters = { ui = true, mounts = false, toys = false, pets = false, map = false }
+    EasyFind.db.uiSearchFilters = { ui = true, mounts = false, toys = false, pets = false, outfits = false, map = false }
     EasyFind.db.uiMapSearchLocal = true
     EasyFind.db.alwaysShowRares = false
     EasyFind.db.optionsPosition = nil
@@ -1843,7 +1843,7 @@ function Options:DoResetUI()
     EasyFind.db.uiResultsWidth = 350
     EasyFind.db.uiSearchPosition = nil
     EasyFind.db.uiResultsHeight = 280
-    EasyFind.db.uiSearchFilters = { ui = true, mounts = false, toys = false, pets = false, map = false }
+    EasyFind.db.uiSearchFilters = { ui = true, mounts = false, toys = false, pets = false, outfits = false, map = false }
     EasyFind.db.uiMapSearchLocal = true
 
     optionsFrame.directOpenCheckbox:SetChecked(false)
@@ -2093,7 +2093,7 @@ function Options:RestoreStandalone()
     optionsFrame:SetBackdropBorderColor(0.50, 0.48, 0.45, 1.0)
 
     optionsFrame:SetParent(UIParent)
-    optionsFrame:SetFrameStrata("MEDIUM")
+    optionsFrame:SetFrameStrata("DIALOG")
     optionsFrame:SetMovable(true)
     optionsFrame:RegisterForDrag("LeftButton")
 
